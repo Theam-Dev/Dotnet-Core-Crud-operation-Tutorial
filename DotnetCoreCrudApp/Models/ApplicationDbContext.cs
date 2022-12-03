7e87b064bcd623fafd59927c06cc24cd8e7575f4
theam-dev <phearakroun@gmail.com>
2022-12-03 18:15:00 +0700

    Dotnet Core Crud

---
+++ DotnetCoreCrudApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotnetCoreCrudApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<Post> Post { get; set; }
+
+    }
+}
---
+++ DotnetCoreCrudApp/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotnetCoreCrudApp.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+    }
+}
